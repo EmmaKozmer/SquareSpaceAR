87222f129d82eaec7ccaf511c4a3d8a0a57a1bca
EmmaKozmer <emma.kozmer@student.unisg.ch>
2024-04-30 21:32:43 +0200

    cleans code

--- js/main.js
+++ js/main.js
@@ -12,3 +12,42 @@ document.addEventListener('DOMContentLoaded', () => {
         location.href = 'about_us.html';
     });
 });
+
+
+function saveImageToLocalStorage(data, name) {
+    var images = JSON.parse(localStorage.getItem('galleryImages')) || {};
+    images[name] = data;
+    localStorage.setItem('galleryImages', JSON.stringify(images));
+}
+
+function loadImagesFromLocalStorage() {
+    var images = JSON.parse(localStorage.getItem('galleryImages')) || {};
+    var dynamicGalleryContainer = document.querySelector('.gallery-container');
+
+    Object.keys(images).forEach(function(name) {
+        var imgElement = document.createElement('img');
+        imgElement.src = images[name];
+        imgElement.alt = name;
+        imgElement.className = 'image'; 
+
+        var divElement = document.createElement('div');
+        divElement.className = 'gallery-item';
+        divElement.appendChild(imgElement);
+
+        var starIcon = document.createElement('div');
+        starIcon.className = 'star-icon';
+        starIcon.innerHTML = '&#9733;';
+        starIcon.onclick = function(event) {
+            toggleFavorite(event, name);
+        };
+        divElement.appendChild(starIcon);
+
+        dynamicGalleryContainer.appendChild(divElement);
+
+        imgElement.onclick = function() {
+            modal.style.display = 'block';
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        };
+    });
+}
